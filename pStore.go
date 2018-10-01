b84531a6775904060785dcd08774d75d13595f8f
inokappa <inokara@gmail.com>
2018-10-01 12:27:41 +0900

    Assume Role の対応の修正

--- pStore.go
+++ pStore.go
@@ -101,6 +101,12 @@ func awsSsmClient(profile string, region string, role string) *ssm.SSM {
         config = aws.Config{Region: aws.String(region),
                             Credentials: creds,
                             Endpoint: aws.String(*argEndpoint)}
+    } else if profile == "" && role != "" {
+        sess := session.Must(session.NewSession())
+        creds := stscreds.NewCredentials(sess, role)
+        config = aws.Config{Region: aws.String(region),
+            Credentials: creds,
+            Endpoint: aws.String(*argEndpoint)}
     } else if profile != "" && role != "" {
         sess := session.Must(session.NewSessionWithOptions(session.Options{Profile:profile}))
         assumeRoler := sts.New(sess)
